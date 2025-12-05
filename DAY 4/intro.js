453b1f28831cb711c44eacf3adee088fd47caefd
Anumitha V <anumitha5831@gmail.com>
2025-12-05 10:02:37 +0530

    added day4

---
+++ DAY 4/intro.js
@@ -0,0 +1,17 @@
+function namefuntion(){
+    console.log("Hello World");
+    console.log("Hello World");
+    console.log("Hello World");
+
+
+}
+namefuntion();
+
+let func = function(){
+    console.log("function is expressed");
+}
+
+let arr= () => {
+    console.log("array is expressed");
+}
+arr();
